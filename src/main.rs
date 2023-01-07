955331b37301ee85a870956dcc52f2c72017883d
akio.yokoyama.yoika@gmail.com <Yoika>
2023-01-07 22:20:40 +0900

    create file command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use structopt::StructOpt;
+use std::fs::File;
+use std::io::Write;
+use std::env;
+use std::path::Path;
+
+#[derive(StructOpt)]
+struct Args {
+    #[structopt(subcommand)]
+    cmd: Option<Command>,
+}
+
+#[derive(StructOpt)]
+enum Command {
+    Memo { name: String, extension: String },
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::from_args_safe()?;
+
+    match args.cmd {
+        Some(Command::Memo { name, extension }) => {
+            let home_path = env::var("HOME").unwrap();
+            let filepath = Path::new(&home_path).join(String::from("Desktop/") + &name + "." + &extension);
+            let mut file = File::create(&filepath)?;
+            file.write_all(String::from("").as_bytes())?;
+        }
+        None => println!("Set arguments."),
+    }
+    Ok(())
 }
