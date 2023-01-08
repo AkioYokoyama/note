e5a85fe4f6900c33a5e29c387297e2f45f78e58b
akio.yokoyama.yoika@gmail.com <Yoika>
2023-01-08 11:32:25 +0900

    Add file open command

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,16 @@ use std::fs::File;
 use std::io::Write;
 use std::env;
 use std::path::Path;
+use std::process::Command;
 
 #[derive(StructOpt)]
 struct Args {
     #[structopt(subcommand)]
-    cmd: Option<Command>,
+    cmd: Option<RustCommand>,
 }
 
 #[derive(StructOpt)]
-enum Command {
+enum RustCommand {
     Memo {
         #[structopt(default_value = "memo")]
         name: String,
@@ -24,11 +25,16 @@ fn main() -> anyhow::Result<()> {
     let args = Args::from_args_safe()?;
 
     match args.cmd {
-        Some(Command::Memo { name, extension }) => {
+        Some(RustCommand::Memo { name, extension }) => {
             let home_path = env::var("HOME").unwrap();
             let filepath = Path::new(&home_path).join(String::from("Desktop/") + &name + "." + &extension);
             let mut file = File::create(&filepath)?;
             file.write_all(String::from("").as_bytes())?;
+
+            Command::new("open")
+                    .arg(&filepath)
+                    .output()
+                    .expect("Failed to open file.");
         }
         None => println!("Set arguments."),
     }
