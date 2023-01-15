f34a719211a086e8ecd3dd00fb87572f89a9b68e
akio.yokoyama.yoika@gmail.com <Yoika>
2023-01-15 18:12:03 +0900

    Delete subcommand

--- src/main.rs
+++ src/main.rs
@@ -7,36 +7,22 @@ use std::process::Command;
 
 #[derive(StructOpt)]
 struct Args {
-    #[structopt(subcommand)]
-    cmd: Option<RustCommand>,
-}
-
-#[derive(StructOpt)]
-enum RustCommand {
-    Memo {
-        #[structopt(default_value = "memo")]
-        name: String,
-        #[structopt(default_value = "md")]
-        extension: String ,
-    },
+    #[structopt(default_value = "memo")]
+    name: String,
+    #[structopt(default_value = "md")]
+    extension: String ,
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::from_args_safe()?;
+    let home_path = env::var("HOME").unwrap();
+    let filepath = Path::new(&home_path).join(String::from("Desktop/") + &args.name + "." + &args.extension);
+    let mut file = File::create(&filepath)?;
+    file.write_all(String::from("").as_bytes())?;
 
-    match args.cmd {
-        Some(RustCommand::Memo { name, extension }) => {
-            let home_path = env::var("HOME").unwrap();
-            let filepath = Path::new(&home_path).join(String::from("Desktop/") + &name + "." + &extension);
-            let mut file = File::create(&filepath)?;
-            file.write_all(String::from("").as_bytes())?;
-
-            Command::new("open")
-                    .arg(&filepath)
-                    .output()
-                    .expect("Failed to open file.");
-        }
-        None => println!("Set arguments."),
-    }
+    Command::new("open")
+            .arg(&filepath)
+            .output()
+            .expect("Failed to open file.");
     Ok(())
 }
